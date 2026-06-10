9992742e1f21cf3247f513a11a15bac3474550d3
ashmithapuchala <ashmithapuchala@gmail.com>
2026-06-10 11:13:08 +0530

    Add hello.java with main method and variables

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello{
+    public static void main(String[]args){
+        int roll=45;
+        double cgpa=7.42;
+        char grade='A';
+        System.out.println(roll);
+        System.out.println(grade);
+        cgpa=8.10;
+        System.out.println(cgpa);
+    }
+}
