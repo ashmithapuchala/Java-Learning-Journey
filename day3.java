e51a729fcc1c5d9fb2544f5e0d8d1388ae94d1e4
ashmithapuchala <ashmithapuchala@gmail.com>
2026-06-11 19:08:55 +0530

    Add hello class with main method for type casting

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        int wholeNumber=75;
+        double automaticDecimal=wholeNumber;
+        System.out.println(automaticDecimal);
+        double realCgpa=8.67;
+        int castedInteger=(int)realCgpa;
+        System.out.println(castedInteger);
+    }
+}
