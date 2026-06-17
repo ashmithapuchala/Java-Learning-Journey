058b536a285926d2ceae7d4f94fe2a3b7ca40103
ashmithapuchala <ashmithapuchala@gmail.com>
2026-06-17 18:45:51 +0530

    Add Main class with student information display

---
+++ day1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentage=19;
+        double studentcgpa=8.75;
+        String studentname="ashmitha";
+        boolean  passed=true;
+        System.out.println("studentage:"+ studentage + "studentcgpa:"+ studentcgpa + "studentname:"+ studentname + "passed:"+ passed );
+        
+    }
+     
+}
